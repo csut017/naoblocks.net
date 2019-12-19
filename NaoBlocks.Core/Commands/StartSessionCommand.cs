de44c3a3dcd294e16aa7558bcddf4b5ac19b722d
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2019-12-19 15:14:02 +1300

    Started work on sessions.

---
+++ NaoBlocks.Core/Commands/StartSessionCommand.cs
@@ -0,0 +1,77 @@
+﻿using NaoBlocks.Core.Models;
+using Newtonsoft.Json;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NaoBlocks.Core.Commands
+{
+    public class StartSessionCommand
+        : OutputCommandBase<Session>
+    {
+        [JsonIgnore]
+        public string Name { get; set; }
+
+        [JsonIgnore]
+        public string Password { get; set; }
+
+        public UserRole Role { get; set; }
+
+        public string UserId { get; set; }
+
+        public async override Task<IEnumerable<string>> ValidateAsync(IAsyncDocumentSession session)
+        {
+            if (session == null) throw new ArgumentNullException(nameof(session));
+            var errors = new List<string>();
+            if (string.IsNullOrWhiteSpace(this.Name))
+            {
+                errors.Add($"User name is required");
+            }
+
+            if (this.Password == null)
+            {
+                errors.Add("Password is required");
+            }
+
+            if (!errors.Any())
+            {
+                var user = await session.Query<User>()
+                    .FirstOrDefaultAsync(u => u.Name == this.Name)
+                    .ConfigureAwait(false);
+                if (user == null)
+                {
+                    errors.Add("Unknown or invalid user");
+                }
+                else if ((user.Password != null) && !user.Password.Verify(this.Password))
+                {
+                    errors.Add("Unknown or invalid user");
+                }
+                else
+                {
+                    this.UserId = user.Id;
+                    this.Role = user.Role;
+                }
+            }
+
+            return errors.AsEnumerable();
+        }
+
+        protected override async Task DoApplyAsync(IAsyncDocumentSession session, CommandResult result)
+        {
+            if (session == null) throw new ArgumentNullException(nameof(session));
+            var newSession = new Session
+            {
+                Role = this.Role,
+                UserId = this.UserId,
+                WhenAdded = this.WhenExecuted,
+                WhenExpires = this.WhenExecuted.AddDays(1)
+            };
+            newSession.GenerateNewKey();
+            await session.StoreAsync(newSession).ConfigureAwait(false);
+            this.Output = newSession;
+        }
+    }
+}
