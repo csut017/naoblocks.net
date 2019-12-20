599288d128c2ec1b7c53b5a0a7c33f81c4dee99f
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2019-12-21 08:35:12 +1300

    Added a CORS policy for development.

--- NaoBlocks.Web/Startup.cs
+++ NaoBlocks.Web/Startup.cs
@@ -44,7 +44,7 @@ namespace NaoBlocks.Web
 
             app.UseStaticFiles();
             app.UseRouting();
-            app.UseCors();
+            app.UseCors("CorsPolicy");
             app.UseWebSockets();
             app.UseAuthentication();
             app.UseAuthorization();
@@ -59,6 +59,15 @@ namespace NaoBlocks.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder
+                .WithOrigins("http://localhost:4200")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+            });
+
             services.AddControllers()
                 .AddJsonOptions(opts =>
                 {
