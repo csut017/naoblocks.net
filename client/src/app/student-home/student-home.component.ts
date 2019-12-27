bd1de555253ea34fd853bf13b8452483db2f809f
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2019-12-27 21:05:12 +1300

    Added the UI for starting a program.

--- client/src/app/student-home/student-home.component.ts
+++ client/src/app/student-home/student-home.component.ts
@@ -8,6 +8,18 @@ import { HomeBase } from '../home-base';
 
 declare var Blockly: any;
 
+class executionStatusStep {
+  image: string = 'circle';
+  title: string;
+  description: string;
+  isCurrent: boolean = false;
+
+  constructor(title: string, description: string) {
+    this.title = title;
+    this.description = description;
+  }
+}
+
 @Component({
   selector: 'app-student-home',
   templateUrl: './student-home.component.html',
@@ -17,6 +29,11 @@ export class StudentHomeComponent extends HomeBase implements OnInit {
 
   aboutOpened: boolean;
   workspace: any;
+  sendingToRobot: boolean = false;
+  steps: executionStatusStep[];
+  startMessage: string;
+  isExecuting: boolean = false;
+  canStop: boolean = false;
 
   constructor(authenticationService: AuthenticationService,
     router: Router) {
@@ -36,5 +53,63 @@ export class StudentHomeComponent extends HomeBase implements OnInit {
       toolbox: xml,
       scrollbars: false
     });
+  }
+
+  doCancelSend(): void {
+    if (this.sendingToRobot) {
+      this.sendingToRobot = false;
+      return;
+    }
+    alert('TODO');
+  }
+
+  doChangeSpeed(): void {
+    alert('TODO');
+  }
+
+  doPlay(): void {
+    this.initialiseStartingUI();
+    this.completeStep(0);
+    this.failStep(1, 'Testing');
+  }
+
+  doStop(): void {
+    alert('TODO');
+  }
+
+  doLoad(): void {
+    alert('TODO');
+  }
+
+  doSave(): void {
+    alert('TODO');
+  }
+
+  private initialiseStartingUI() {
+    this.steps = [
+      new executionStatusStep('Check Program', 'Checks that the program is valid and can run on the robot.'),
+      new executionStatusStep('Select Robot', 'Finds an available robot to run the program on.'),
+      new executionStatusStep('Send to Robot', 'Sends the program to the robot.'),
+      new executionStatusStep('Start Execution', 'Starts the program running on the robot.')
+    ];
+    this.steps[0].isCurrent = true;
+    this.startMessage = undefined;
+    this.sendingToRobot = true;
+  }
+
+  private completeStep(step: number) {
+    if (step >= this.steps.length) return;
+    this.steps[step].isCurrent = false;
+    this.steps[step].image = 'success-standard';
+
+    if (++step >= this.steps.length) return;
+    this.steps[step].isCurrent = true;
+  }
+
+  private failStep(step: number, reason: string) {
+    this.startMessage = reason;
+    if (step >= this.steps.length) return;
+    this.steps[step].isCurrent = false;
+    this.steps[step].image = 'error-standard';
   }
 }
