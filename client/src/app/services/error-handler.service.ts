76ccb49790803ae2998b39154b8c7aaeee351098
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2019-12-29 16:19:56 +1300

    Fixed client error messages.

--- client/src/app/services/error-handler.service.ts
+++ client/src/app/services/error-handler.service.ts
@@ -9,8 +9,8 @@ export class ErrorHandlerService {
 
   formatError(data: any): string {
     if (data.error) {
-      if (data.error.validationErrors) return data.error.validationErrors.join()
-      if (data.error.executionErrors) return data.error.executionErrors.join()
+      if (data.error.validationErrors) return data.error.validationErrors.map(e => e.error).join()
+      if (data.error.executionErrors) return data.error.executionErrors.map(e => e.error).join()
     }
     return data.message;
   }
