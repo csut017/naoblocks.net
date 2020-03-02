bf574025a77eb50398fa74d7d3d2a53ef4ed52e5
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2020-03-03 08:57:48 +1300

    Added script for building and copying the client

---
+++ generateClient.cmd
@@ -0,0 +1,13 @@
+@echo off
+echo Building Client
+cd client
+call ng build --prod
+cd ..
+
+echo Cleaning old folder
+del /F/Q/S NaoBlocks.Web\wwwroot\*.* > NUL
+rmdir /Q/S NaoBlocks.Web\wwwroot
+
+echo Copying to server folder
+mkdir /Q/S NaoBlocks.Web\wwwroot
+xcopy client\dist\client\*.* NaoBlocks.Web\wwwroot\ /s
