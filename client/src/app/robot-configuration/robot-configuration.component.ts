f18b3322d0800b39cac903fb72e52b6921152aaf
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2020-06-23 15:40:50 +1200

    Provide list of robot types that can be configured

--- client/src/app/robot-configuration/robot-configuration.component.ts
+++ client/src/app/robot-configuration/robot-configuration.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SystemService } from '../services/system.service';
 import { environment } from 'src/environments/environment';
+import { RobotTypeService } from '../services/robot-type.service';
+import { RobotType } from '../data/robot-type';
 
 @Component({
   selector: 'app-robot-configuration',
@@ -9,19 +11,32 @@ import { environment } from 'src/environments/environment';
 })
 export class RobotConfigurationComponent implements OnInit {
 
-  public addresses: string[] = [];
-  public connectTextUrl: string = '';
-  public downloading: boolean = true;
+  addresses: string[] = [];
+  robotTypes: RobotType[] = [];
+  connectTextUrl: string = '';
+  downloadingAddresses: boolean = true;
+  downloadingRobotTypes: boolean = true;
 
-  constructor(private systemService: SystemService) { }
+  constructor(private systemService: SystemService,
+    private robotTypeService: RobotTypeService) { }
 
   ngOnInit(): void {
     this.connectTextUrl = `${environment.apiURL}v1/system/addresses/connect.txt`;
     this.systemService.listRobotAddresses()
       .subscribe(result => {
         this.addresses = result.items;
-        this.downloading = false;
+        this.downloadingAddresses = false;
       });
+      this.robotTypeService.list()
+      .subscribe(data => {
+        this.robotTypes = data.items;
+        this.downloadingRobotTypes = false;
+      });
+  }
+
+  generateDownloadPackageUrl(robotType: RobotType): string {
+    const url = `${environment.apiURL}v1/robots/types/${robotType.name}/package`;
+    return url;
   }
 
 }
