a51fbcdda654d85bc7834e163d37fce0a8d49567
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2020-10-06 11:21:35 +1300

    Bug fix: robot mock missing constants

--- robot/Nao/noRobot.py
+++ robot/Nao/noRobot.py
@@ -3,6 +3,24 @@ import logger
 class RobotMock(object):
     ''' Fakes a robot instance. This is used primarily for testing the rest of the components on a PC. '''
 
+    SIT = 'Sit'
+    STAND = 'Stand'
+    CROUCH = 'Crouch'
+
+    LIEONBACK = 'LyingBack'
+    LIEONBELLY = 'LyingBelly'
+    SITBACK = 'SitRelax'
+    SITFORWARD = 'Sit'
+    STANDINIT = 'StandInit'
+    STANDZERO = 'StandZero'
+
+    RIGHT_EYE = 1
+    LEFT_EYE = 2
+    CHEST = 4
+
+    RIGHT_HAND = 1
+    LEFT_HAND = 2
+
     def __getattr__(self, name):
         def _missing(*args, **kwargs):
             logger.log('[Fake] Called method "%s"', name)
