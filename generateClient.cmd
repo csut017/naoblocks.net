0002a7cd4fc5757caa3a801b2c5ce7c6eb3931a7
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2021-05-26 15:23:27 +1200

    Further work on the initialisation

--- generateClient.cmd
+++ generateClient.cmd
@@ -5,9 +5,9 @@ call ng build --prod
 cd ..
 
 echo Cleaning old folder
-del /F/Q/S NaoBlocks.Web\wwwroot\*.* > NUL
+del /F/Q/S NaoBlocks.Web\wwwroot\*.* 
 rmdir /Q/S NaoBlocks.Web\wwwroot
 
 echo Copying to server folder
 mkdir /Q/S NaoBlocks.Web\wwwroot
-xcopy client\dist\client\*.* NaoBlocks.Web\wwwroot\ /s
+xcopy client\dist\client\*.* NaoBlocks.Web\wwwroot\ /S
