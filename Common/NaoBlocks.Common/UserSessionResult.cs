7b5f6443300822182fcf432766e6473f7318f2a3
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2021-11-22 21:34:21 +1300

    Ported additional classes.

---
+++ Common/NaoBlocks.Common/UserSessionResult.cs
@@ -0,0 +1,23 @@
+﻿namespace NaoBlocks.Common
+{
+    /// <summary>
+    /// Contains the result of starting or resuming a user session.
+    /// </summary>
+    public class UserSessionResult
+    {
+        /// <summary>
+        /// Gets or sets the date and time the session expires (in UTC.)
+        /// </summary>
+        public DateTime Expires { get; set; } = DateTime.MinValue;
+
+        /// <summary>
+        /// Gets or sets the role of the user.
+        /// </summary>
+        public string Role { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the session token.
+        /// </summary>
+        public string Token { get; set; } = string.Empty;
+    }
+}
