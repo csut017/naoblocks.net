e2eb1ab974e579db9b364ccbe566d0ac4f8d21a1
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2021-12-22 15:19:43 +1300

    Added robot log DTOs.

---
+++ Server/NaoBlocks.Web/Dtos/RobotLog.cs
@@ -0,0 +1,64 @@
+﻿using Data = NaoBlocks.Engine.Data;
+
+namespace NaoBlocks.Web.Dtos
+{
+    /// <summary>
+    /// A Data Transfer Object for a robot log.
+    /// </summary>
+    public class RobotLog
+    {
+        /// <summary>
+        /// Gets or sets the conversation id.
+        /// </summary>
+        public long ConversationId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user name.
+        /// </summary>
+        public string? UserName { get; set; }
+
+        /// <summary>
+        /// Gets the lines for the log.
+        /// </summary>
+        public IList<RobotLogLine>? Lines { get; private set; }
+
+        /// <summary>
+        /// Gets or sets when the log was added.
+        /// </summary>
+        public DateTime WhenAdded { get; set; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// Gets or sets when the log was updated.
+        /// </summary>
+        public DateTime WhenLastUpdated { get; set; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// Converts a database entity to a Data Transfer Object.
+        /// </summary>
+        /// <param name="value">The database entity.</param>
+        /// <param name="includeLines">Whether to include the lines or not.</param>
+        /// <param name="converstion">The associated conversation.</param>
+        /// <returns>A new <see cref="RobotLog"/> instance containing the required properties.</returns>
+        public static RobotLog FromModel(Data.RobotLog value, bool includeLines, Data.Conversation? converstion)
+        {
+            var robotLog = new RobotLog
+            {
+                ConversationId = value.Conversation.ConversationId,
+                UserName = value.Conversation.UserName,
+                WhenAdded = value.WhenAdded,
+                WhenLastUpdated = value.WhenLastUpdated
+            };
+            if (converstion != null)
+            {
+                robotLog.ConversationId = converstion.ConversationId;
+                robotLog.UserName = converstion.UserName;
+            }
+            if (includeLines)
+            {
+                robotLog.Lines = value.Lines.Select(RobotLogLine.FromModel).ToList();
+            }
+
+            return robotLog;
+        }
+    }
+}
---
+++ Tests/NaoBlocks.Web.Tests/Dtos/RobotLogLineTests.cs
@@ -0,0 +1,50 @@
+﻿using Transfer = NaoBlocks.Web.Dtos;
+using Data = NaoBlocks.Engine.Data;
+using Xunit;
+using System;
+using System.Linq;
+using NaoBlocks.Common;
+
+namespace NaoBlocks.Web.Tests.Dtos
+{
+    public class RobotLogLineTests
+    {
+        [Fact]
+        public void FromModelConvertsEntity()
+        {
+            var now = DateTime.Now;
+            var entity = new Data.RobotLogLine
+            {
+                Description = "Kia ora",
+                SourceMessageType = ClientMessageType.Authenticated,
+                WhenAdded = now
+            };
+            var dto = Transfer.RobotLogLine.FromModel(entity);
+            Assert.Equal("Kia ora", dto.Description);
+            Assert.Equal(ClientMessageType.Authenticated, dto.SourceMessageType);
+            Assert.Equal(now, dto.WhenAdded);
+            Assert.Null(dto.Values);
+        }
+
+        [Fact]
+        public void FromModelConvertsEntityWithValues()
+        {
+            var entity = new Data.RobotLogLine
+            {
+                Description = "Kia ora",
+                SourceMessageType = ClientMessageType.Authenticated,
+                WhenAdded = DateTime.Now
+            };
+            entity.Values.Add(new Data.NamedValue { Name = "tahi", Value = "rua" });
+            entity.Values.Add(new Data.NamedValue { Name = "toru" });
+            var dto = Transfer.RobotLogLine.FromModel(entity);
+            Assert.NotNull(dto.Values);
+            Assert.Equal(
+                new[] { "tahi=rua", "toru=" }, 
+                dto.Values!
+                    .Select(nv => $"{nv.Key}={nv.Value}")
+                    .OrderBy(v => v)
+                    .ToArray());
+        }
+    }
+}
