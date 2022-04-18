c9bb481883ef64418b10db1b2a2c601d6e077be6
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2022-04-19 09:59:22 +1200

    Fixed broken unit tests: * Client required an empty string rather than an empty XML document

--- Tests/NaoBlocks.Engine.Tests/Generators/UserToolboxTests.cs
+++ Tests/NaoBlocks.Engine.Tests/Generators/UserToolboxTests.cs
@@ -9,135 +9,6 @@ namespace NaoBlocks.Engine.Tests.Generators
 {
     public class UserToolboxTests : DatabaseHelper
     {
-        [Theory]
-        [InlineData(ReportFormat.Unknown, false)]
-        [InlineData(ReportFormat.Zip, false)]
-        [InlineData(ReportFormat.Pdf, false)]
-        [InlineData(ReportFormat.Excel, false)]
-        [InlineData(ReportFormat.Text, true)]
-        [InlineData(ReportFormat.Csv, false)]
-        public void IsFormatAvailableChecksAllowedTypes(ReportFormat format, bool allowed)
-        {
-            var generator = new UserToolbox();
-            Assert.Equal(allowed, generator.IsFormatAvailable(format));
-        }
-
-        [Fact]
-        public async Task GenerateAsyncThrowsErrorIfNoDefaultRobot()
-        {
-            // Arrange
-            var user = new User { Id = "users/1", Name = "Mia" };
-            using var store = InitialiseDatabase(
-                user);
-            using var session = store.OpenAsyncSession();
-            var generator = InitialiseGenerator<UserToolbox>(session);
-
-            // Act
-            var error = await Assert.ThrowsAsync<ApplicationException>(async () => await generator.GenerateAsync(ReportFormat.Text, user));
-
-            // Assert
-            Assert.Equal("Cannot determine robot type", error.Message);
-        }
-
-        [Fact]
-        public async Task GenerateAsyncThrowsErrorIfInvalidRobotAllocated()
-        {
-            // Arrange
-            var user = new User { Id = "users/1", Name = "Mia" };
-            user.Settings.RobotTypeId = "types/1";
-            using var store = InitialiseDatabase(
-                user);
-            using var session = store.OpenAsyncSession();
-            var generator = InitialiseGenerator<UserToolbox>(session);
-
-            // Act
-            var error = await Assert.ThrowsAsync<ApplicationException>(async () => await generator.GenerateAsync(ReportFormat.Text, user));
-
-            // Assert
-            Assert.Equal("Unknown robot type", error.Message);
-        }
-
-        [Fact]
-        public async Task GenerateAsyncGeneratesMinimalWithSystemRobot()
-        {
-            // Arrange
-            var user = new User { Id = "users/1", Name = "Mia" };
-            var robotType = new RobotType { IsDefault = true };
-            using var store = InitialiseDatabase(
-                user,
-                robotType);
-            using var session = store.OpenAsyncSession();
-            var generator = InitialiseGenerator<UserToolbox>(session);
-
-            // Act
-            var output = await generator.GenerateAsync(ReportFormat.Text, user);
-
-            // Assert
-            Assert.Equal("Toolbox-Mia.xml", output.Item2);
-            using var reader = new StreamReader(output.Item1);
-            var text = await reader.ReadToEndAsync();
-            Assert.Equal(
-                @"<?xml version=""1.0"" encoding=""utf-8""?><xml />",
-                text);
-        }
-
-        [Fact]
-        public async Task GenerateAsyncGeneratesMinimalWithUserRobot()
-        {
-            // Arrange
-            var user = new User { Id = "users/1", Name = "Mia" };
-            user.Settings.RobotTypeId = "types/1";
-            var robotType = new RobotType { Id = "types/1", IsDefault = true };
-            using var store = InitialiseDatabase(
-                user,
-                robotType);
-            using var session = store.OpenAsyncSession();
-            var generator = InitialiseGenerator<UserToolbox>(session);
-
-            // Act
-            var output = await generator.GenerateAsync(ReportFormat.Text, user);
-
-            // Assert
-            Assert.Equal("Toolbox-Mia.xml", output.Item2);
-            using var reader = new StreamReader(output.Item1);
-            var text = await reader.ReadToEndAsync();
-            Assert.Equal(
-                @"<?xml version=""1.0"" encoding=""utf-8""?><xml />",
-                text);
-        }
-
-        [Fact]
-        public async Task GenerateAsyncGeneratesSimpleWithSystemRobot()
-        {
-            // Arrange
-            var user = new User { Id = "users/1", Name = "Mia" };
-            user.Settings.Simple = true;
-            var robotType = new RobotType { IsDefault = true };
-            robotType.Toolbox.Add(
-                GenerateCategory("Simple", "simple",
-                    GenerateBlock("tahi", "<rua/>", 3)));
-            robotType.Toolbox.Add(
-                GenerateCategory("Default", "default",
-                    GenerateBlock("whitu", "<waru/>", 9),
-                    GenerateBlock("wha", "<rima/>", 6)));
-            using var store = InitialiseDatabase(
-                user,
-                robotType);
-            using var session = store.OpenAsyncSession();
-            var generator = InitialiseGenerator<UserToolbox>(session);
-
-            // Act
-            var output = await generator.GenerateAsync(ReportFormat.Text, user);
-
-            // Assert
-            Assert.Equal("Toolbox-Mia.xml", output.Item2);
-            using var reader = new StreamReader(output.Item1);
-            var text = await reader.ReadToEndAsync();
-            Assert.Equal(
-                @"<?xml version=""1.0"" encoding=""utf-8""?><xml><category name=""Simple"" colour=""1""><rua /></category></xml>",
-                text);
-        }
-
         [Fact]
         public async Task GenerateAsyncGeneratesCustomWithSystemRobot()
         {
@@ -175,13 +46,94 @@ namespace NaoBlocks.Engine.Tests.Generators
         }
 
         [Fact]
+        public async Task GenerateAsyncGeneratesEmptyWithSystemRobot()
+        {
+            // Arrange
+            var user = new User { Id = "users/1", Name = "Mia" };
+            var robotType = new RobotType { IsDefault = true };
+            using var store = InitialiseDatabase(
+                user,
+                robotType);
+            using var session = store.OpenAsyncSession();
+            var generator = InitialiseGenerator<UserToolbox>(session);
+
+            // Act
+            var output = await generator.GenerateAsync(ReportFormat.Text, user);
+
+            // Assert
+            Assert.Equal("Toolbox-Mia.xml", output.Item2);
+            using var reader = new StreamReader(output.Item1);
+            var text = await reader.ReadToEndAsync();
+            Assert.Equal(
+                string.Empty,
+                text);
+        }
+
+        [Fact]
+        public async Task GenerateAsyncGeneratesEmptyWithUserRobot()
+        {
+            // Arrange
+            var user = new User { Id = "users/1", Name = "Mia" };
+            user.Settings.RobotTypeId = "types/1";
+            var robotType = new RobotType { Id = "types/1", IsDefault = true };
+            using var store = InitialiseDatabase(
+                user,
+                robotType);
+            using var session = store.OpenAsyncSession();
+            var generator = InitialiseGenerator<UserToolbox>(session);
+
+            // Act
+            var output = await generator.GenerateAsync(ReportFormat.Text, user);
+
+            // Assert
+            Assert.Equal("Toolbox-Mia.xml", output.Item2);
+            using var reader = new StreamReader(output.Item1);
+            var text = await reader.ReadToEndAsync();
+            Assert.Equal(
+                string.Empty,
+                text);
+        }
+
+        [Fact]
+        public async Task GenerateAsyncGeneratesSimpleWithSystemRobot()
+        {
+            // Arrange
+            var user = new User { Id = "users/1", Name = "Mia" };
+            user.Settings.Simple = true;
+            var robotType = new RobotType { IsDefault = true };
+            robotType.Toolbox.Add(
+                GenerateCategory("Simple", "simple",
+                    GenerateBlock("tahi", "<rua/>", 3)));
+            robotType.Toolbox.Add(
+                GenerateCategory("Default", "default",
+                    GenerateBlock("whitu", "<waru/>", 9),
+                    GenerateBlock("wha", "<rima/>", 6)));
+            using var store = InitialiseDatabase(
+                user,
+                robotType);
+            using var session = store.OpenAsyncSession();
+            var generator = InitialiseGenerator<UserToolbox>(session);
+
+            // Act
+            var output = await generator.GenerateAsync(ReportFormat.Text, user);
+
+            // Assert
+            Assert.Equal("Toolbox-Mia.xml", output.Item2);
+            using var reader = new StreamReader(output.Item1);
+            var text = await reader.ReadToEndAsync();
+            Assert.Equal(
+                @"<?xml version=""1.0"" encoding=""utf-8""?><xml><category name=""Simple"" colour=""1""><rua /></category></xml>",
+                text);
+        }
+
+        [Fact]
         public async Task GenerateAsyncGeneratesWithAllOptions()
         {
             // Arrange
             var user = new User { Id = "users/1", Name = "Mia" };
             user.Settings.Conditionals = true;
             user.Settings.Dances = true;
-            user.Settings.Events= true;
+            user.Settings.Events = true;
             user.Settings.Loops = true;
             user.Settings.Sensors = true;
             user.Settings.Variables = true;
@@ -210,6 +162,54 @@ namespace NaoBlocks.Engine.Tests.Generators
             Assert.Equal(
                 @"<?xml version=""1.0"" encoding=""utf-8""?><xml><category name=""Conditionals"" colour=""1"" /><category name=""Dances"" colour=""1"" /><category name=""Default"" colour=""1"" /><category name=""Events"" colour=""1"" /><category name=""Loops"" colour=""1"" /><category name=""Sensors"" colour=""1"" /><category name=""Variables"" colour=""1"" /></xml>",
                 text);
+        }
+
+        [Fact]
+        public async Task GenerateAsyncThrowsErrorIfInvalidRobotAllocated()
+        {
+            // Arrange
+            var user = new User { Id = "users/1", Name = "Mia" };
+            user.Settings.RobotTypeId = "types/1";
+            using var store = InitialiseDatabase(
+                user);
+            using var session = store.OpenAsyncSession();
+            var generator = InitialiseGenerator<UserToolbox>(session);
+
+            // Act
+            var error = await Assert.ThrowsAsync<ApplicationException>(async () => await generator.GenerateAsync(ReportFormat.Text, user));
+
+            // Assert
+            Assert.Equal("Unknown robot type", error.Message);
+        }
+
+        [Fact]
+        public async Task GenerateAsyncThrowsErrorIfNoDefaultRobot()
+        {
+            // Arrange
+            var user = new User { Id = "users/1", Name = "Mia" };
+            using var store = InitialiseDatabase(
+                user);
+            using var session = store.OpenAsyncSession();
+            var generator = InitialiseGenerator<UserToolbox>(session);
+
+            // Act
+            var error = await Assert.ThrowsAsync<ApplicationException>(async () => await generator.GenerateAsync(ReportFormat.Text, user));
+
+            // Assert
+            Assert.Equal("Cannot determine robot type", error.Message);
+        }
+
+        [Theory]
+        [InlineData(ReportFormat.Unknown, false)]
+        [InlineData(ReportFormat.Zip, false)]
+        [InlineData(ReportFormat.Pdf, false)]
+        [InlineData(ReportFormat.Excel, false)]
+        [InlineData(ReportFormat.Text, true)]
+        [InlineData(ReportFormat.Csv, false)]
+        public void IsFormatAvailableChecksAllowedTypes(ReportFormat format, bool allowed)
+        {
+            var generator = new UserToolbox();
+            Assert.Equal(allowed, generator.IsFormatAvailable(format));
         }
 
         private static ToolboxBlock GenerateBlock(string name, string definition, int order)
