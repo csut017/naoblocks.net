eac4a0330e59afb7c0ec9631fa5532d7de93dd68
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2022-06-03 16:24:21 +1200

    Initial client deployment.

---
+++ deployClient.cmd
@@ -0,0 +1,14 @@
+@echo off
+echo Building Client
+cd Clients\Angular
+call ng build
+cd ..
+cd ..
+
+echo Cleaning old folder
+del /F/Q/S Server\NaoBlocks.Web\wwwroot\*.* 
+rmdir /Q/S Server\NaoBlocks.Web\wwwroot
+
+echo Copying to server folder
+mkdir Server\NaoBlocks.Web\wwwroot
+xcopy Clients\Angular\dist\angular\*.* Server\NaoBlocks.Web\wwwroot\ /S
