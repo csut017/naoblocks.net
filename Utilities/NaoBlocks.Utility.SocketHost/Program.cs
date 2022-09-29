3f69579b08288cf427013758b1354ef74a698052
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2022-09-29 18:11:13 +1300

    Built single client socket listener

---
+++ Utilities/NaoBlocks.Utility.SocketHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+
+namespace NaoBlocks.Utility.SocketHost
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            // var address = ClientAddressList.RetrieveAddresses().FirstOrDefault()
+            //     ?? IPAddress.Loopback;
+            var address = IPAddress.Loopback;
+            var endpoint = new IPEndPoint(
+                address,
+                5010);
+            var server = new SocketListener();
+            server.MessageReceived += (o, e) =>
+            {
+                Console.WriteLine($"Received {e.Type}");
+            };
+
+            Console.WriteLine($"Listening on {endpoint}");
+            Console.WriteLine("Press Ctrl-C to exit");
+            await server.StartAsync(endpoint);
+        }
+    }
+}
---
+++ Utilities/NaoBlocks.Utility.SocketHost/SocketListener.cs
@@ -0,0 +1,112 @@
+﻿using NaoBlocks.Common;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NaoBlocks.Utility.SocketHost
+{
+    /// <summary>
+    /// Listener for socket connections.
+    /// </summary>
+    public class SocketListener
+    {
+        private readonly CancellationTokenSource tokenSource = new();
+
+        /// <summary>
+        /// Fired whenever a message is received.
+        /// </summary>
+        public event EventHandler<ClientMessage>? MessageReceived;
+
+        /// <summary>
+        /// Cancels listening.
+        /// </summary>
+        public void Cancel()
+        {
+            tokenSource.Cancel();
+        }
+
+        /// <summary>
+        /// Starts listening on the specified port.
+        /// </summary>
+        /// <param name="endPoint">The end point to listen on.</param>
+        public async Task StartAsync(IPEndPoint endPoint)
+        {
+            using Socket listener = new(
+                endPoint.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            listener.Bind(endPoint);
+            listener.Listen();
+            var handler = await listener.AcceptAsync(this.tokenSource.Token);
+            int messageType = 0;
+            int sequenceNum = 0;
+            var isInData = false;
+            var receivedData = new byte[1_024];
+            var dataPos = 0;
+            while (handler.Connected && !this.tokenSource.IsCancellationRequested)
+            {
+                var buffer = new byte[1_024];
+                var received = await handler.ReceiveAsync(buffer, SocketFlags.None, this.tokenSource.Token);
+                if (received == 0)
+                {
+                    await handler.DisconnectAsync(false);
+                }
+                else
+                {
+                    for (var loop = 0; loop < received; loop++)
+                    {
+                        var character = buffer[loop];
+                        receivedData[dataPos++] = character;
+                        if (isInData)
+                        {
+                            if (character != 0) continue;
+                            GenerateInternalMessage(messageType, receivedData, dataPos);
+                            isInData = false;
+                            dataPos = 0;
+                        }
+                        else
+                        {
+                            if (dataPos < 4) continue;
+
+                            messageType = receivedData[1];
+                            messageType = receivedData[0] + (messageType << 256);
+                            sequenceNum = receivedData[3];
+                            sequenceNum = receivedData[2] + (receivedData[3] << 256);
+                            isInData = true;
+                            dataPos = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void GenerateInternalMessage(int messageType, byte[] receivedData, int dataPos)
+        {
+            var message = new ClientMessage((ClientMessageType)messageType);
+            if (dataPos > 0)
+            {
+                var values = Encoding
+                    .UTF8
+                    .GetString(receivedData, 0, dataPos - 1)
+                    .Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                foreach (var value in values)
+                {
+                    var equalPos = value.IndexOf('=');
+                    if (equalPos < 0)
+                    {
+                        message.Values.Add(value, string.Empty);
+                    }
+                    else
+                    {
+                        var key = value[0..equalPos];
+                        var thisValue = value[(equalPos + 1)..];
+                        message.Values.Add(key, thisValue);
+                    }
+                }
+            }
+
+            this.MessageReceived?.Invoke(this, message);
+        }
+    }
+}
