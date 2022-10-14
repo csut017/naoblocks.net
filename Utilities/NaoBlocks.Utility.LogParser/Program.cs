060cf9440b3b408e04e5d65387c434f4bb9647a4
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2022-10-15 07:30:57 +1300

    Added errors and formatting to the log parsing

--- Utilities/NaoBlocks.Utility.LogParser/Program.cs
+++ Utilities/NaoBlocks.Utility.LogParser/Program.cs
@@ -1,6 +1,7 @@
 ﻿using NaoBlocks.Common;
 using NaoBlocks.Parser;
 using OfficeOpenXml;
+using OfficeOpenXml.Table;
 
 namespace NaoBlocks.Utility.LogParser
 {
@@ -30,6 +31,7 @@ namespace NaoBlocks.Utility.LogParser
                 Directory.CreateDirectory(outputFolder);
             }
 
+            // Start the output and add the headers
             using var outputPackage = new ExcelPackage();
             var outputWorksheet = outputPackage.Workbook.Worksheets.Add("Programs");
             outputWorksheet.Cells[1, 1].Value = "Person";
@@ -37,12 +39,15 @@ namespace NaoBlocks.Utility.LogParser
             outputWorksheet.Cells[1, 3].Value = "Name";
             outputWorksheet.Cells[1, 4].Value = "When Added";
             outputWorksheet.Cells[1, 5].Value = "Code";
+            outputWorksheet.Cells[1, 6].Value = "Error";
 
+            // Load the input worksheet
             WriteLine($"Loading data from {inputPath}");
             using var inputPackage = new ExcelPackage();
             await inputPackage.LoadAsync(inputPath);
             var inputWorksheet = inputPackage.Workbook.Worksheets.First();
             var count = 0;
+            var maxColumn = 0;
             var displayOptions = new AstNode.DisplayOptions
             {
                 Children = AstNode.DisplayType.Ignore,
@@ -52,6 +57,7 @@ namespace NaoBlocks.Utility.LogParser
             };
             for (var row = 2; row <= inputWorksheet.Dimension.End.Row; row++)
             {
+                // Parse each program and output to the output sheet
                 inputWorksheet.Cells[row, 1, row, 5].Copy(outputWorksheet.Cells[row, 1]);
                 WriteLine($"-> Compiling code in row {row}");
 
@@ -60,14 +66,29 @@ namespace NaoBlocks.Utility.LogParser
                 if (result.Errors.Any())
                 {
                     WriteLine($"-> Unable to compile: {result.Errors.First()}");
+                    outputWorksheet.Cells[row, 6].Value = result.Errors.First().ToString();
                     continue;
                 }
 
-                WriteNodes(outputWorksheet, row, 6, result.Nodes[1], displayOptions);
+                var last = WriteNodes(outputWorksheet, row, 7, result.Nodes[1], displayOptions) - 7;
+                if (last > maxColumn) maxColumn = last;
                 count++;
             }
-
             WriteLine($"-> Processed {count} rows");
+
+            // Add the column headers
+            for (var loop = 0; loop < maxColumn; loop++)
+            {
+                outputWorksheet.Cells[1, loop + 7].Value = $"Block {loop + 1}";
+            }
+
+            // Add an Excel table
+            var dimensions = outputWorksheet.Dimension.End;
+            var tableRange = outputWorksheet.Cells[1, 1, dimensions.Row, dimensions.Column];
+            var table = outputWorksheet.Tables.Add(tableRange, "DataTable");
+            table.TableStyle = TableStyles.Light1;
+
+            // Save the output worksheet
             WriteLine($"Saving to {outputPath}");
             outputWorksheet.Cells[outputWorksheet.Dimension.Address].AutoFitColumns();
             await outputPackage.SaveAsAsync(outputPath);
