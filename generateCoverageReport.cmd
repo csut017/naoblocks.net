a6392480fbd3580c06d3198f1e969a406ddcb487
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2022-12-04 14:11:57 +1300

    Updated code coverage script

--- generateCoverageReport.cmd
+++ generateCoverageReport.cmd
@@ -1,3 +1,15 @@
+@echo off
+
+echo Cleaning up
 del /s /q coverage.cobertura.xml
+
+echo Running tests
 dotnet test --collect:"XPlat Code Coverage"
+
+echo Generating reports
 reportgenerator -reports:Tests\*\TestResults\*\coverage.cobertura.xml -targetdir:Tests\Coverage -historydir:Tests\History -reporttypes:Html;TextDeltaSummary
+
+if %1 equ open ( 
+    echo Opening coverage report
+    Tests\Coverage\index.htm
+)
